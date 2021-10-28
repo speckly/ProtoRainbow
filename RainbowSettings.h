75cced2a42e27cf253c7324c6135e43c587896d0
Jonathan Ong <60218942+cheetawolf@users.noreply.github.com>
2021-10-28 11:13:12 +0800

    sine animations

--- RainbowSettings.h
+++ RainbowSettings.h
@@ -2,7 +2,13 @@
 int imageHeight = 32; //Your matrix height
 int imageWidth = 64; //Your matrix width
 int pixelCount = imageHeight * imageWidth;
-int wavelength = 3; //Value of rgb waves shown in one frame, idk how to explain lol, default value is 1
-int frame_delay = 15;
-int animation_delay = 8;
+int wavelength = 2; //Value of rgb waves shown in one frame, idk how to explain lol, default value is 1
+int frame_delay = 10;
+int animation_delay = 4;
 int loopEvent = 2;
+int xsine_period = 4; //(360/b)
+int xsine_amp = 3; //mulitiplier
+int xsine_phase = 0 
+int xsine_offset = 0;; 
+int ysine_period = 3; 
+int ysine_amp = 4;
